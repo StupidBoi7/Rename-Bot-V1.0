3df4ce18bdb041cdcbf758c368bca2bd9b31a588
StupidBoi7 <126649764+StupidBoi7@users.noreply.github.com>
2023-06-12 11:05:11 +0530

    Update config.py
    Signed-off-by: StupidBoi7 <126649764+StupidBoi7@users.noreply.github.com>

--- config.py
+++ config.py
@@ -2,17 +2,17 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "25695562")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "0b691c3e86603a7e34aae0b5927d725a")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6280390519:AAGQOMWfbTJqpDeKs85EoHsWQTyrPFFElPw") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "WebXBots") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "AnimeDownloaderChat_Bot", "Overflow_s") 
 
-DB_NAME = os.environ.get("DB_NAME","")     
+DB_NAME = os.environ.get("DB_NAME","mongodb+srv://vidoso7560:vidoso7560@cluster0.odwizw5.mongodb.net/?retryWrites=true&w=majority")     
 
-DB_URL = os.environ.get("DB_URL","")
+DB_URL = os.environ.get("DB_URL","cluster0")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
